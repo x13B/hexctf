a327f5c292424a880c370e4cadb474f864bd8865
imacias10 <imacias1@csub.edu>
2023-09-12 15:42:27 -0700

    Testing CRUD operations with db.

--- hexctf/src/lib/db.server.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Centralized database client.
-// Anytime a connection is needed, import:
-// lib/db.server.js
-export const db = new PrismaClient();
--- hexctf/src/routes/create/+page.svelte
+++ hexctf/src/routes/create/+page.svelte
@@ -1,1 +1,29 @@
+<script lang="ts">
+    import { PrismaClient } from '@prisma/client';
+    
+    const db = new PrismaClient();
+
+    /*
+     * This is a test function to get data from the database.
+     * It will print the only line of data from  the database as a dictionary.
+     * It prints an error message in the browser but not the terminal.
+     */
+    async function getUsers() {
+        const user = await db.user.findMany();
+        console.log(user);
+        return true;
+    }
+
+    /* Call the function 
+     * Prints success because function returns true.
+     * If some error occurred then it would return false.
+     */
+    getUsers()
+    .then(async () =>{
+        console.log("success");})
+    .catch(async () => {
+        console.log("error");});
+
+</script>
+
 <h1>This is the create an account page.</h1>
