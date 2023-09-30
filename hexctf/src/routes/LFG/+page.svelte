49f5afe6922f10bd406e9b1bd023b806c0da02d9
imacias10 <imacias1@csub.edu>
2023-09-29 21:45:53 -0700

    Working on sort functions

--- hexctf/src/routes/LFG/+page.svelte
+++ hexctf/src/routes/LFG/+page.svelte
@@ -27,43 +27,10 @@
   
   // Hides input field when submit button is clicked
   let inputVisible: boolean = true;
+
+  // Holds the teams for the competition
+  let teamsMadeBySort: any = [];
   
-  // This function will easy sort the users into groups
-  // TODO: continue working on this function
-  function sortGroups(groups: any, numGroups: number) {
-    // console.log("Sorting teams");
-    // let officialTeams = Array.from({length: numGroups}, () => []);
-
-    // // Filter out admin
-    // let teams = groups.filter((group:any) => group.score != -1);
-
-    // let front = 0;
-    // let back = teams.length - 1;
-    // console.log("Starting Sort")
-    // while (front <= back) {
-    //   console.log("First sort");
-    //   for (let i = 0; i < officialTeams.length; i++) {
-    //     console.log("Pushing front:", teams[front]);
-    //     officialTeams[i].push(teams[front]);
-    //     front++;
-    //   }
-    //   console.log(officialTeams)
-    //   console.log("Second sort");
-    //   for (let j = officialTeams.length - 1; j >= 0; j--) { // Corrected line
-    //     console.log("Pushing", teams[back]);
-    //     officialTeams[j].push(teams[back]);
-    //     back--;
-    //   }
-    //   console.log(officialTeams)
-    //   break;
-    // }
-    // console.log("End Sort")
-
-    // return teams;
-  }
-
-
-
   // This gets all users in the database
   onMount(async () => {
     try {
@@ -99,8 +66,6 @@
         // Filters out the scores that are less than 0
         const filteredScores = userScores.filter((user:any) => user.score > 0);
         userScores = filteredScores;
-
-        console.log("from function user", userScores);
       } else {
         console.error("Failed to get users", res.status);
       }
@@ -108,19 +73,86 @@
       console.error("Error fetching data:", error);
     }
   })
+ 
+  // This function will easy sort the users into groups
+  // TODO: continue working on this function
+  function sortGroups(groups: any, numGroups: number) {
+    // console.log("Sorting teams");
+    // let officialTeams = Array.from({length: numGroups}, () => []);
+
+    // // Filter out admin
+    // let teams = groups.filter((group:any) => group.score != -1);
+
+    // let front = 0;
+    // let back = teams.length - 1;
+    // console.log("Starting Sort")
+    // while (front <= back) {
+    //   console.log("First sort");
+    //   for (let i = 0; i < officialTeams.length; i++) {
+    //     console.log("Pushing front:", teams[front]);
+    //     officialTeams[i].push(teams[front]);
+    //     front++;
+    //   }
+    //   console.log(officialTeams)
+    //   console.log("Second sort");
+    //   for (let j = officialTeams.length - 1; j >= 0; j--) { // Corrected line
+    //     console.log("Pushing", teams[back]);
+    //     officialTeams[j].push(teams[back]);
+    //     back--;
+    //   }
+    //   console.log(officialTeams)
+    //   break;
+    // }
+    // console.log("End Sort")
+
+    // return teams;
+  }
 
   function easySort() {
     console.log("easy sort working");
   }
 
-  function randomSort() {
-    console.log("random sort working");
-  }
+  // Using Fisher-Yates algorithm to randomly sort
+  const shuffleArray = (array:any) => {
+    // Create a shallow copy of the original array
+    const shuffledArray = [...array]; 
 
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      // Swap the elements in the shuffledArray
+      const temp = shuffledArray[i];
+      shuffledArray[i] = shuffledArray[j];
+      shuffledArray[j] = temp;
+    }
+
+    // Return the shuffled array
+    return shuffledArray;
+  };
+
+  function randomSort() {
+    let shuffledTeams = shuffleArray(userScores);
+    let index: number = 0;
+    const membersPerTeam: number = Math.floor(userScores.length / numGroups);
+    
+    
+    for (let i = 0; i < numGroups; i++) {
+      let team: any = [];
+
+      for (let j = 0; j < 2; j++) {
+        team.push(shuffledTeams[index]);
+        index++;
+      }
+      teamsMadeBySort.push(team);
+    }
+
+    console.log("teams", teamsMadeBySort);
+  }
+  
   function complexFairSort() {
     console.log("Complex fair sort working");
   }
-
+    
   // This hides the submit button to avoid changing group number
   function hideButton() {
     groupsButtonVisible = false;
@@ -147,11 +179,14 @@
 
 <br>
 
+<!-- Check to see if the number of groups is valid before sorting teams -->
 <div>
-  <strong>Select a sorting method:</strong>
-  <button on:click={easySort}>Easy Sort</button>
-  <button on:click={randomSort}>Random Sort</button>
-  <button on:click={complexFairSort}>Complex Fair Sort</button>
+  {#if numGroups > 0}
+    <strong>Select a sorting method:</strong>
+    <button on:click={easySort}>Easy Sort</button>
+    <button on:click={randomSort}>Random Sort</button>
+    <button on:click={complexFairSort}>Complex Fair Sort</button>
+  {/if}
 </div>
 
 <!-- This will be removed when login functionality works -->
@@ -173,3 +208,22 @@
       <li>User Score: {score.UserId}: {score.score}</li>
     {/each}
 </div>
+
+{#if teamsMadeBySort.length > 0}
+  <!-- <strong>Teams have not been defined</strong> -->
+<!-- {:else} -->
+  <div>
+    <strong>Current Teams</strong>
+    {teamsMadeBySort}
+    {#each teamsMadeBySort as teamArray}
+      <div>
+        <h2>Team:</h2>
+        <ul>
+          {#each teamArray as player}
+            <li>{player.name}</li>
+          {/each}
+        </ul>
+      </div>
+    {/each}
+  </div>
+{/if}
