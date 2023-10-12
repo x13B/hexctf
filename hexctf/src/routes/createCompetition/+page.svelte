ed99e5b9ade237f918474df3385564fb42275d51
imacias10 <98487954+imacias10@users.noreply.github.com>
2023-10-12 11:21:53 -0700

    Removed unused code

--- hexctf/src/routes/createCompetition/+page.svelte
+++ hexctf/src/routes/createCompetition/+page.svelte
@@ -2,117 +2,114 @@
   import { onMount } from "svelte";
   
   let showForm = false;
-    let selectedCategories: string[];
-    let categories: any[];
-    let newCategory: string;
-    let catName: string;
-    let numQuestions: number;
-    let start: Date;
-    let end: Date;
-    let data_loaded: boolean = false;
-    let showQuizForm: boolean = false;
-    let questions: any[] = [];
-    let question_body: string = '';
-    let question_answer: string = '';
-    let questionId: number = 1;
-    let categorySelected: string = '';
+  let categories: any[];
+  let newCategory: string;
+  let catName: string;
+  let start: Date;
+  let end: Date;
+  let data_loaded: boolean = false;
+  let showQuizForm: boolean = false;
+  let questions: any[] = [];
+  let question_body: string = '';
+  let question_answer: string = '';
+  let questionId: number = 1;
+  let categorySelected: string = '';
 
-    onMount(async () => {
-      try {
-        const res = await fetch('../api/getCategories');
-        if (res.ok) {
-          categories = await res.json();      
-          console.log(categories);
-          data_loaded = true;
+  onMount(async () => {
+    try {
+      const res = await fetch('../api/getCategories');
+      if (res.ok) {
+        categories = await res.json();      
+        console.log(categories);
+        data_loaded = true;
 
-        } else {
-          console.error("Failed to get categories", res.status);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      } else {
+        console.error("Failed to get categories", res.status);
       }
-    })
-
-    const displayForm = () => {
-        showForm = true;
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  })
 
-    // Let's you add new categories without overwriting the old ones.
-    const createNewCategory = async () => {
-      const newCategoryObj = {
-        CategoryId: categories.length + 1,
-        CategoryName: newCategory,
-      };
+  const displayForm = () => {
+      showForm = true;
+  }
 
-      // Add the category to your local array
-      categories.push(newCategoryObj);
-      // Trigger a reactivity update by assigning a new array
-      categories = [...categories];
-
-      // Clear the input field after adding the category
-      newCategory = '';
-
-      let id: number = newCategoryObj.CategoryId;
-      let name: string = newCategoryObj.CategoryName;
-
-      try {
-        const res = await fetch('/api/addCategory', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({id, name}),
-        });
-
-        if (res.ok) {
-          console.log('Category added successfully to the database');
-          // You can optionally fetch the updated list of categories from the server
-          // and update your local categories array here.
-        } else {
-          console.error('Failed to add category to the database:', res.statusText);
-        }
-      } catch (error) {
-        console.error('Error adding category to the database:', error);
-      }
+  // Let's you add new categories without overwriting the old ones.
+  const createNewCategory = async () => {
+    const newCategoryObj = {
+      CategoryId: categories.length + 1,
+      CategoryName: newCategory,
     };
 
-    async function submitOptions() {
-      console.log(selectedCategories);
-      console.log("Startime: ", start);
-      console.log("End time: ", end);
-      console.log(catName, selectedCategories, numQuestions, start, end);
+    // Add the category to your local array
+    categories.push(newCategoryObj);
+    // Trigger a reactivity update by assigning a new array
+    categories = [...categories];
 
-      showQuizForm = true;
-        // console.log(selectedCategories);
-        // const res = await fetch('../api/addCategory', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type' : 'application/json'
-        //   },
-        //   body: JSON.stringify({ })
-        // });
+    // Clear the input field after adding the category
+    newCategory = '';
 
-        // if (res.ok) {
-        //   console.log("Succesfully added category");
-        // } else {
-        //   console.error("Someting happened: ", res);
-        // }
+    let id: number = newCategoryObj.CategoryId;
+    let name: string = newCategoryObj.CategoryName;
+
+    try {
+      const res = await fetch('/api/addCategory', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id, name}),
+      });
+
+      if (res.ok) {
+        console.log('Category added successfully to the database');
+        // You can optionally fetch the updated list of categories from the server
+        // and update your local categories array here.
+      } else {
+        console.error('Failed to add category to the database:', res.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding category to the database:', error);
     }
+  };
 
-    const addQuestion = () => {
-      questions = [
-        ...questions,
-        { id: questionId, body: question_body, answer: question_answer, category: categorySelected }
-      ];
+  async function submitOptions() {
+    console.log("Startime: ", start);
+    console.log("End time: ", end);
+    console.log(catName, start, end);
 
-      questions.sort((a, b) => a.category.localeCompare(b.category));
-      
-      console.log(questions);
+    showQuizForm = true;
+      // console.log(selectedCategories);
+      // const res = await fetch('../api/addCategory', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type' : 'application/json'
+      //   },
+      //   body: JSON.stringify({ })
+      // });
 
-      questionId++; // Increment the questionId
-      question_body = '';
-      question_answer = '';
-    }
+      // if (res.ok) {
+      //   console.log("Succesfully added category");
+      // } else {
+      //   console.error("Someting happened: ", res);
+      // }
+  }
+
+  const addQuestion = () => {
+    questions = [
+      ...questions,
+      { id: questionId, body: question_body, answer: question_answer, category: categorySelected }
+    ];
+
+    questions.sort((a, b) => a.category.localeCompare(b.category));
+    
+    console.log(questions);
+
+    questionId++; // Increment the questionId
+    question_body = '';
+    question_answer = '';
+  }
 </script>
 
 <h1>This is the create competition page</h1>
@@ -126,18 +123,13 @@
         
         <label for="category">Categories:</label><br>
         {#each categories as category (category.CategoryId)}
-            <label>
-                <input type="checkbox" name="cat-options" bind:group={selectedCategories} value={category.CategoryId}/>
                 {category.CategoryName}
-            </label><br>
+            <br>
         {/each}
         
         <label for="add">Create New Category: </label>
         <input type="text" name="new-cat" bind:value={newCategory}/>
         <button type="button" on:click={createNewCategory}>Create New</button><br>
-
-        <label for="num-questions">Number of Questions per Category:</label>
-        <input type="number" bind:value={numQuestions}/><br>
         
         <label for="length">Start Date:</label>
         <input type="datetime-local" bind:value={start}/><br>
@@ -192,5 +184,7 @@
         </label>
       {/each}
       <button type="submit" on:click={addQuestion}>Add Question</button>
+      <br>
+      <button type="submit">Submit Quiz</button>
     </form>
 {/if}
