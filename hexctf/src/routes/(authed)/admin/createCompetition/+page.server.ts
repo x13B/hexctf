25be10c01306125298094de1719508832026290c
imacias10 <imacias1@csub.edu>
2023-11-11 14:39:43 -0800

    Added timer in create comp page

--- hexctf/src/routes/(authed)/admin/createCompetition/+page.server.ts
+++ hexctf/src/routes/(authed)/admin/createCompetition/+page.server.ts
@@ -6,8 +6,11 @@ export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) throw redirect(302, "/login");
 
+	const comp = await prisma.competition.findFirst();
+
 	return {
 	  userId: session.user.userId,
 	  username: session.user.username,
+	  comp: comp,
 	};
   };
