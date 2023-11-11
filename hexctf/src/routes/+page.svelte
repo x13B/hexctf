9eb4cadbadef73bb0c5213148b0fd4d0f725815a
imacias10 <imacias1@csub.edu>
2023-11-10 21:33:36 -0800

    splitting create comp to multiple pages

--- hexctf/src/routes/+page.svelte
+++ hexctf/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <script lang="ts">
 	import { username_when_logged_in, check_if_admin, users_id } from "./(authed)/admin/createCompetition/username";
 	import { enhance } from "$app/forms";
-	import { onMount } from "svelte";
+	// import { onMount } from "svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
@@ -13,24 +13,24 @@
 	username_when_logged_in.set(data.username);
 	users_id.set(data.userId);
 	
-	// This will check if the user is an admin to set the flag true
-	onMount(async () => {
-    try {
-      const response = await fetch('../api/getUsers'); 
-      if (response.ok) {
-        let users = await response.json();
-		console.log(users);
+// 	// This will check if the user is an admin to set the flag true
+// 	onMount(async () => {
+//     try {
+//       const response = await fetch('../api/getUsers'); 
+//       if (response.ok) {
+//         let users = await response.json();
+// 		console.log(users);
 
-		if (data.username === users.id && users.isAdmin === true) {
-		check_if_admin.set(true);
-		}
-      } else {
-        console.error('Failed to fetch data:', response.status);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  });
+// 		if (data.username === users.id && users.isAdmin === true) {
+// 		check_if_admin.set(true);
+// 		}
+//       } else {
+//         console.error('Failed to fetch data:', response.status);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   });
 
 </script>
 
