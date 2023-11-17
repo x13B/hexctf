c5f9f493d4e2fb85e4949df5a1797aa132b12ac7
iruiz10 <iruiz10@csub.edu>
2023-11-16 17:14:06 -0800

    hints show up properly on the Questions page

--- hexctf/src/routes/questions/[slug]/+page.svelte
+++ hexctf/src/routes/questions/[slug]/+page.svelte
@@ -6,7 +6,9 @@
     const  question  = data.question;
     const  check  = data.check;
     export let form: ActionData;
-    let hintClicked:boolean = false
+    let hintClicked:boolean = false;
+    let hintClicked2:boolean = false;
+    let hintClicked3:boolean = false;
    
 
 
@@ -17,10 +19,26 @@
       <h2>{question?.title}</h2>
       <p>{question?.description}</p>
       <button on:click|once={() => hintClicked = true}>
-        Need a hint?
+        Hint
       </button>
       {#if hintClicked}
         <p>{question?.hint}</p>
+      {/if}
+      {#if question.hint2 != ""}
+        <button on:click|once={() => hintClicked2 = true}>
+          Hint 2
+        </button>
+      {/if}
+      {#if hintClicked2}
+        <p>{question?.hint2}</p>
+      {/if}
+      {#if question.hint3 != ""}
+        <button on:click|once={() => hintClicked3 = true}>
+          Hint 3
+        </button>
+      {/if}
+      {#if hintClicked3}
+        <p>{question?.hint3}</p>
       {/if}
       <p>{question?.points} Points</p>
       {#if check.resp}
