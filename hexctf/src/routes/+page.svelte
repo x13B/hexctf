55345054ec4577a60f04505706fab68fead99129
imacias10 <98487954+imacias10@users.noreply.github.com>
2023-12-04 09:31:09 -0800

    fixing create a comp page

--- hexctf/src/routes/+page.svelte
+++ hexctf/src/routes/+page.svelte
@@ -3,6 +3,7 @@
   
 	export let data: PageData;
 	let competition = data.competition;
+	let desc: string = (data.comp_timer == null) ? '' : data.comp_timer.description;
   </script>
 
 <svelte:head>
@@ -11,3 +12,4 @@
 
 <h1>{competition.competitionName}</h1>
 <h2>Welcome</h2>
+<p>{desc}</p>
