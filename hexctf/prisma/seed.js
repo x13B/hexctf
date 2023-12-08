6b0367e2315eb9cc831c8e64da5957c2520cbea0
imacias10 <imacias1@csub.edu>
2023-12-07 17:57:19 -0800

    removed 2 student data

--- hexctf/prisma/seed.js
+++ hexctf/prisma/seed.js
@@ -18,7 +18,7 @@ async function seed() {
     // ========================================================================
 
     // THIS IS FOR ADDING NEW USERS ==========================================
-    let userList = ["Student 1","Student 2","Student 3","Student 4","Student 5","Student 6","Student 7","Student 8","Student 9"];
+    let userList = ["Student 1","Student 2","Student 3","Student 4","Student 5","Student 6","Student 7"];
     let num = 1;
     for (let nameuser of userList) {
       //console.log(nameuser)
